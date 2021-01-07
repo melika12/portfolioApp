be34f21be56b272f40e754a7af2eade4a6b006ee
Melika Emma Vigh Kjærgaard Pedersen <meli0970@edu.mercantec.dk>
2021-01-07 12:14:09 +0100

    project

---
+++ portfolioApp/portfolioApp/ContactPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Plugin.Messaging;
+using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace portfolioApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContactPage : ContentPage
+    {
+        public ICommand TapCommand => new Command<string>(async (url) => await Launcher.OpenAsync(url));
+        public ContactPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+        private void Email(object sender, EventArgs e)
+        {
+            var email = CrossMessaging.Current.EmailMessenger;
+            if (email.CanSendEmail)
+                email.SendEmail("dontlook@me.org", "Email from app", "This is a test email from the app");
+
+            /*Alternate / more difficult way: 
+            var email = CrossMessaging.Current.EmailMessenger;
+            if (email.CanSendEmail) {
+                var message = new EmailMessageBuilder()
+                    .To("dontlook@me.org")
+                    .Subject("Email from app")
+                    .Body("This is a test email from the app")
+                    .Build();
+
+                email.SendEmail(message);
+            }
+            */
+        }
+        private void Call(object sender, EventArgs e)
+        {
+            var phone = CrossMessaging.Current.PhoneDialer;
+            if (phone.CanMakePhoneCall)
+                phone.MakePhoneCall("+4512345678", "Melika Pedersen");
+        }
+        private void Message(object sender, EventArgs e)
+        {
+            var message = CrossMessaging.Current.SmsMessenger;
+            if (message.CanSendSms)
+                message.SendSms("+4512345678", "Test message from app");
+        }
+    }
+}
---
+++ portfolioApp/portfolioApp/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace portfolioApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void About(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AboutPage());
+        }
+        private async void Education(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new EducationPage());
+        }
+        private async void Experience(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ExperiencePage());
+        }
+        private async void Contact(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ContactPage());
+        }
+    }
+}
